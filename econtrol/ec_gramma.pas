3c3e83b8e17392ac680edd57be6cca356629cd7e
Alexey <support@uvviewsoft.com>
2021-03-15 14:14:55 +0300

    restore old optim

--- econtrol/ec_gramma.pas
+++ econtrol/ec_gramma.pas
@@ -302,22 +302,19 @@ var Lex: TecSyntAnalyzer;
   end;
 
   procedure ReadRepeater(RuleItem: TParserRuleItem);
-  var
-    s: ecString;
   begin
     if not ValidCur then Exit;
     if Res.Tags[Cur].TokenType = 8 then
      begin
+       {
        s := Res.TagStr[Cur];
        if s = '+' then RuleItem.RepMax := -1 else
         if s = '?' then RuleItem.RepMin := 0 else
          if s = '*' then
-       {
-       //seems ok optimization, but need to find the reason of AppManagerThread bug
+         }
        if Res.TagSameAs(Cur, '+') then RuleItem.RepMax := -1 else
         if Res.TagSameAs(Cur, '?') then RuleItem.RepMin := 0 else
          if Res.TagSameAs(Cur, '*') then
-         }
           begin
            RuleItem.RepMin := 0;
            RuleItem.RepMax := -1;
