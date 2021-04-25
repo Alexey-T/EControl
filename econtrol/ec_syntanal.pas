0763b17ca3ace74fb961775f1c287590206d6e90
Alextp <aaa5500@ya.ru>
2021-04-25 17:44:30 +0300

    cln

--- econtrol/ec_syntanal.pas
+++ econtrol/ec_syntanal.pas
@@ -4198,8 +4198,11 @@ begin
     ClearClientContents;
   end;
   UpdateClients;
+  {
+  //Alexey: removed
   for i := 0 to FClientList.Count - 1 do
-   TecClientSyntAnalyzer(FClientList[i]).EventParseNeeded.SetEvent;
+    TecClientSyntAnalyzer(FClientList[i]).EventParseNeeded.SetEvent;
+    }
 end;
 
 procedure TecSyntAnalyzer.HighlightKeywords(Client: TecParserResults;
