b4fab34fadd87a287187a32a122ff4a1cd1dcea5
Alexey <support@uvviewsoft.com>
2023-05-25 22:33:13 +0300

    hide errors about missing 'PHP_' sublexer

--- econtrol/ec_lexerlist.pas
+++ econtrol/ec_lexerlist.pas
@@ -175,7 +175,8 @@ begin
           if Assigned(anSub) then
             an.SubAnalyzers[j].SyntAnalyzer:= anSub
           else
-          if an.LexerName<>'HTML' then //ignore the HTML lexer which uses 'VBScript' 2 times
+          if (an.LexerName<>'HTML') //ignore the HTML lexer which uses 'VBScript'
+            and (LexName<>'PHP_') then
             AErrorLines+= Format('Lexer "%s" needs sub-lexer "%s" which was not installed'#10, [an.LexerName, LexName]);
         end;
       end;
